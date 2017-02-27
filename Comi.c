5bfefabc9676a60a00c10059a04c610dd0651f16
Алымбаев Тимур Русланович <ip612s01@awp05.csc.local>
2017-02-27 09:15:39 +0700

    Dobavil function vichislenie

--- Comi.c
+++ Comi.c
@@ -26,6 +26,49 @@ if (sr < 0)
  return ch;
 }
 
+int vich (int sum, int sr)
+{
+if (sum < 100000)
+{
+if (sr < 31 && 0 <= sr)
+{  
+    den = sum - sum / 100 * 10;                      
+    }  
+if (sr < 121 && 31 <= sr)
+{  
+    den = sum + sum / 100 * 2;                   
+    }  
+if (sr < 241 && 121 <= sr)
+{  
+    den = sum + sum / 100 * 6;                   
+    }   
+if (sr < 366 && 241 <= sr)
+{  
+    den = sum + sum / 100 * 12;                     
+    }                    
+    }
+if (sum > 100000)
+{
+if (sr < 31 && 0 <= sr)
+{  
+    den = sum - sum / 100 * 10;         
+    }  
+if (sr < 121 && 31 <= sr)
+{  
+    den = sum + sum / 100 * 3;                   
+    }  
+if (sr < 241 && 121 <= sr)
+{  
+    den = sum + sum / 100 * 8;                    
+    }   
+if (sr < 366 && 241 <= sr)
+{  
+    den = sum + sum / 100 * 15;              
+    } 
+    }
+    return den;
+}
+
 int main()
 {
   next:
